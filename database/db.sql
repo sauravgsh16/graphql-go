2c800129f92652a81535a0e265094d531484974d
Saurav Ghosh <sauravgsh16@gmail.com>
2019-11-29 04:42:45 +0530

    sql ops added

--- database/db.sql
+++ database/db.sql
@@ -1,22 +1,23 @@
-CREATE TABLE authors (
+DROP TABLE books;
+DROP TABLE authors;
+
+CREATE TABLE IF NOT EXISTS authors (
     ID SERIAL PRIMARY KEY,
     NAME TEXT NOT NULL,
     AGE TEXT
 );
 
-CREATE TABLE books (
+CREATE TABLE IF NOT EXISTS books (
     ID SERIAL PRIMARY KEY,
     NAME TEXT NOT NULL,
     GENRE TEXT NOT NULL,
-    AUTHOR_ID INTEGER REFERENCES authors (ID)
+    AUTHOR_ID INTEGER,
+    FOREIGN KEY (AUTHOR_ID) REFERENCES authors (ID)
 );
 
-
-insert into authors (NAME, Age) values ('AAA', 42);
-insert into authors (NAME, Age) values ('BBB', 52);
-insert into authors (NAME, Age) values ('CCC', 62);
-
-
+insert into authors (NAME, Age) values ('John', 42);
+insert into authors (NAME, Age) values ('Dave', 52);
+insert into authors (NAME, Age) values ('Frank', 62);
 
 insert into books (NAME, GENRE, AUTHOR_ID) values ('First Book', 'Fantasy', 1);
 insert into books (NAME, GENRE, AUTHOR_ID) values ('Second Book', 'Tech', 2);
--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"github.com/sauravgsh16/graphql-go/database"
 	"github.com/sauravgsh16/graphql-go/server"
 )
 
 func main() {
+	// Init DB tables
+	for _, qry := range readSQL() {
+		database.MustExec(qry)
+	}
+	// Start server
 	server.StartApp()
+}
+
+func readSQL() []string {
+	content, err := ioutil.ReadFile("./database/db.sql")
+	if err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+	var lines []string
+	for _, l := range strings.Split(string(content), ";") {
+		lines = append(lines, fmt.Sprintf("%s;", strings.TrimSpace(l)))
+	}
+	return lines
 }
